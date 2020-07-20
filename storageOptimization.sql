6f05cbf5a60e6d8afabb975c8b218cb7d30b0bf9
Chuhan Ran <ranchuhan@gmail.com>
2020-07-20 12:09:41 -0700

    codesignal_db_65

---
+++ storageOptimization.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE storageOptimization()
+BEGIN
+
+SELECT id, 'name' AS column_name, name AS value FROM workers_info
+WHERE name IS NOT NULL
+UNION
+SELECT id, 'date_of_birth', date_of_birth FROM workers_info
+WHERE date_of_birth IS NOT NULL
+UNION
+SELECT id, 'salary', salary FROM workers_info
+WHERE salary IS NOT NULL
+ORDER BY id, FIELD(Column_name, 'name', 'date_of_birth', 'salary');	
+END
